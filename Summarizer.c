894ccecb391f886914cef6f359c333a47f1a1bbc
penghat <ananth.penghat@gmail.com>
2017-06-10 14:22:05 -0700

    Read in input from a text file
    Leave the paste in reading for later
    Remove extra method for reading in

--- Summarizer.c
+++ Summarizer.c
@@ -2,24 +2,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sysexits.h>
+#include "Summarizer.h"
 
-int main() {
 
-  printf("Welcome. This Summarizer will summarize an article.\n");
-  while (1) {   
-    int type;
+int main(int argc, char *argv[]) {
 
-    printf("Enter '1' to process plain text, "
-           "'2' for a file, and anything else to exit: ");
-    scanf(" %d", &type);
-    if (type == 1) {
-      printf("Paste the text here: \n");
-    } else if (type == 2) {
-      printf("Type the name of the file here: \n");
-    } else {
-      exit(0);
+  FILE *input;
+  Word_Map *map[3000]; /* Array of pointers to structs (max 3000 words) */
+  char *sentences[100]; /* Hold all sentences of article in order */
+  char text[5000]; /* Array that holds read in paragraphs */
+
+  if (argc == 1) {
+    /* inplement pasting into terminal case later */
+
+  } else if (argc == 2) {
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+      fprintf(stderr, "%s cannot be opened.\n", argv[1]);
+      exit(EX_OSERR);
     }
+    while (!feof(input)) { /* Read in each paragraph */
+      fgets(text, 15000, input);
+      /* process each paragraph */
+    }
+  } else { /* Incorrect input */
+    fprintf(stderr, "Usage: a.out");
+    fprintf(stderr, "Usage: a.out <filename>\n");
+    exit(EX_USAGE);
   }
   return 0;
 }
-
