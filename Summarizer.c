4f65bcb2f9913538fb4494a04d4fa93a3cef6a23
penghat <ananth.penghat@gmail.com>
2017-06-13 18:36:49 -0700

    Seperate individual sentences from paragraphs
    Some problems in handling special cases for punctuation:
    1) Abbreviations are treated as seperate sentences (ex: U.S = 2
    sentences)
    2) Doesn't include end quotation mark if end punctuation precedes
    it ("Hello?" said the man = "Hello?)

--- Summarizer.c
+++ Summarizer.c
@@ -2,16 +2,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <sysexits.h>
 #include "Summarizer.h"
 
+static void get_sentences(char *[], char[], int * count);
 
 int main(int argc, char *argv[]) {
 
-  FILE *input;
+  FILE * input;
   Word_Map *map[3000]; /* Array of pointers to structs (max 3000 words) */
-  char *sentences[100]; /* Hold all sentences of article in order */
-  char text[5000]; /* Array that holds read in paragraphs */
+  char * sentences[100]; /* Hold all (100) sentences of article in order */
+  char text[5000]; /* Array that holds read in paragraphs (max 5000 chars) */
+  int sentence_count = 0; /* number of sentences */
 
   if (argc == 1) {
     /* inplement pasting into terminal case later */
@@ -22,14 +26,37 @@ int main(int argc, char *argv[]) {
       fprintf(stderr, "%s cannot be opened.\n", argv[1]);
       exit(EX_OSERR);
     }
-    while (!feof(input)) { /* Read in each paragraph */
-      fgets(text, 15000, input);
+    while (fgets(text, 15000, input) != NULL) { /* Read in each paragraph */
       /* process each paragraph */
+      get_sentences(sentences, text, &sentence_count);  
     }
+    printf("number of sentences: %d\n", sentence_count);
   } else { /* Incorrect input */
     fprintf(stderr, "Usage: a.out");
     fprintf(stderr, "Usage: a.out <filename>\n");
     exit(EX_USAGE);
   }
   return 0;
+}
+
+static void get_sentences(char * arr[], char * text, int * count) {
+  char sentence[5000]; /* Holds the sentence */
+  int i, j = 0, length = strlen(text);
+
+  for (i = 0; i < length; i++) {
+    while (isspace(text[i]) && j == 0) { /* Skip over preceding whitespace */
+        i++;
+    }
+    sentence[j++] = text[i]; /* Copy text over */
+
+    /* End of sentence */
+    if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+      sentence[j] = '\0'; /* Null character to make string */
+      j = 0;
+      (*count)++; /* Increment # of sentences */
+      printf("%s\n\n", sentence);
+      memset(sentence, 0, sizeof(sentence)); 
+    }
+  }
+
 }
