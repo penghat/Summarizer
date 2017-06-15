cb17d82796bf88637b261c276934d4054e725145
penghat <ananth.penghat@gmail.com>
2017-06-15 15:50:45 -0700

    Fix more formatting issues
    Make all single line comments the same style: '//'
    Fix spacing
    Remove whitespace

--- Summarizer.c
+++ Summarizer.c
@@ -19,9 +19,8 @@ int main(int argc, char *argv[]) {
   int sentence_count = 0, word_count = 0; // # unique sentences/words
   int i;
 
-
   if (argc == 1) {
-    /* inplement pasting into terminal case later */
+    // inplement pasting into terminal case later 
 
   } else if (argc == 2) {
     input = fopen(argv[1], "r");
@@ -30,16 +29,15 @@ int main(int argc, char *argv[]) {
       exit(EX_OSERR);
     }
     while (fgets(text, 15000, input) != NULL) { // Read in each paragraph
-      // Process each paragraph
-      get_sentences(sentences, text, &sentence_count);
+      get_sentences(sentences, text, &sentence_count); // Process paragraph
     }
-    for (i = 0; i < sentence_count; i++) { // Process each sentence
+    for (i = 0; i < sentence_count; i++) { // Process sentence
       get_words(map, sentences[i], &word_count);
     }
     /*for (int f = 0; f < word_count; f++) {
       printf("word: %s frequency: %d\n", (map[f])->word, map[f]->count);
     }*/
-  } else { /* Incorrect input */
+  } else { // Incorrect input
     fprintf(stderr, "Usage: a.out");
     fprintf(stderr, "Usage: a.out <filename>\n");
     exit(EX_USAGE);
@@ -49,7 +47,7 @@ int main(int argc, char *argv[]) {
 
 static void get_sentences(char arr[][5000], char * text, int * count) {
 
-  char sentence[5000]; /* Holds the sentence */
+  char sentence[5000]; // Holds the sentence
   int i, j = 0, length = strlen(text), quotes_seen = 0;
 
   for (i = 0; i < length; i++) {
@@ -59,13 +57,13 @@ static void get_sentences(char arr[][5000], char * text, int * count) {
 
     // If current character is a quotation mark
     if (text[i] == '\"' || text[i] == -100 || text[i] == -99) {
-      quotes_seen++;
+      quotes_seen++; // Keep track to ensure open AND close quotes added
     }
     sentence[j++] = text[i]; // Copy text over normally otherwise
 
-    /* End of sentence */
+    // End of sentence 
     if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
-      if (quotes_seen % 2 != 0) { // Not all quotes added to sentence
+      if (quotes_seen % 2 != 0) { // If not all quotes added to sentence
         i++; // Move to next char
 
         while ((text[i] != '\"' || text[i] != -100 || text[i] != -99)
@@ -78,7 +76,7 @@ static void get_sentences(char arr[][5000], char * text, int * count) {
       (*count)++; // Increment # of sentences
       quotes_seen = 0; // Reset quote counters
       j = 0; // Reset counter for individual sentences
-      strcpy(arr[*count - 1],sentence); // Store sentence in array of sentences
+      strcpy(arr[*count - 1],sentence); // Store sentence in array 
       memset(sentence, 0, sizeof(sentence));  // Clear sentence
     }
   }
@@ -89,13 +87,14 @@ static void get_words(Word_Map * map[], char * text, int * count) {
   int i, j = 0, length = strlen(text), word_found = 0;
   char word[25];
 
-  for (i = 0; i < length; i++) { /* Get individual words */
+  for (i = 0; i < length; i++) { // Get individual words 
 
     while (isalpha(text[i]) || isdigit(text[i]) ||
            text[i] == '-' || text[i] == -103) {
       word[j++] = text[i++];
     }
-    if (isspace(text[i])) {
+
+    if (isspace(text[i])) { // End of word reached
       for (int k = 0; k < *count; k++) { // See if word already added
         if (strcasecmp((map[k])->word, word) == 0) {
             (map[k])->count++;  // Increment frequency
@@ -108,6 +107,7 @@ static void get_words(Word_Map * map[], char * text, int * count) {
         (map[*count])->count = 1;
         (*count)++; // Increment # of unique words
       }
+
       word_found = 0; // Reset boolean that indicates if word already added
       j = 0;
       memset(word, 0, sizeof(word)); // Clear word
